e22ba64f8c08b26f4b5bc90b2d158f20936290c4
Mithuna Kodithuwakku <144697486+MithunaDevinda@users.noreply.github.com>
2023-09-22 11:10:13 +0000

    final

--- web/apps/text-polish/app/convert/page.tsx
+++ web/apps/text-polish/app/convert/page.tsx
@@ -3,17 +3,32 @@ import './App.css';
 
 
 export default function() {
+
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var textArea = document.getElementById("textArea");
+    
+    function getText() {
+                var text = textArea.value;
+    
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+                ctx.font = "10px Arial";
+                ctx.fillStyle = "black";
+                ctx.fillText(text, 10, 50);
+    }
+
     return (
         <div>
-                <div class="container">
-        <div class="left-side">
+                <div className="container">
+        <div className="left-side">
             <textarea id="textArea"></textarea>
         </div>
-        <div class="right-side">
-            <canvas id="myCanvas" class="canvasS"></canvas>
+        <div className="right-side">
+            <canvas id="myCanvas" className="canvasS"></canvas>
         </div>
     </div>
-    <p class="buttonR"> <button class="button" onclick="getText()">Generate</button></p>
+    <p className="buttonR"> <button className="button" onclick="getText()">Generate</button></p>
         </div>
     )
-}
+} 
