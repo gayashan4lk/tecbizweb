ffd07293f693657e76df9a8f9e71a0913c2f4da5
Mithuna Kodithuwakku <144697486+MithunaDevinda@users.noreply.github.com>
2023-10-03 08:43:45 +0000

    useEffect

--- web/apps/text-polish/app/convert/page.tsx
+++ web/apps/text-polish/app/convert/page.tsx
@@ -1,34 +1,44 @@
-import react from "react"
+//import React from "react";
+import React, { useEffect } from "react"; //new line for line 1
 import './App.css';
 
+export default function CanvasTextGenerator() {
+  //useEffect(() => {  //new line
+  function getText() { //new line 2nd round
+  var canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+  const ctx = canvas?.getContext("2d");
+  const  textArea = document.getElementById("textArea") as HTMLTextAreaElement;
 
-export default function() {
+  //function getText() {
+    if (ctx && textArea) {
+      var text = textArea.value;
 
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    var textArea = document.getElementById("textArea");
-    
-    function getText() {
-                var text = textArea.value;
-    
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-                ctx.font = "10px Arial";
-                ctx.fillStyle = "black";
-                ctx.fillText(text, 10, 50);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      ctx.font = "10px Arial";
+      ctx.fillStyle = "black";
+      ctx.fillText(text, 10, 50);
     }
+  }
+            return (() => {  //new line
+            
+           // };   //new line //removed 2nd round
+          }, []);   //new line
 
-    return (
-        <div>
-                <div className="container">
+
+  return (
+    <div>
+      <div className="container">
         <div className="left-side">
-            <textarea id="textArea"></textarea>
+          <textarea id="textArea" className="textarea"></textarea>  //class names added
         </div>
         <div className="right-side">
-            <canvas id="myCanvas" className="canvasS"></canvas>
+          <canvas id="myCanvas" className="canvasS"></canvas>  //class names added
         </div>
+      </div>
+      <p className="buttonR">
+        <button className="button" onClick={getText}>Generate</button>
+      </p>
     </div>
-    <p className="buttonR"> <button className="button" onclick="getText()">Generate</button></p>
-        </div>
-    )
-} 
+  );
+}
