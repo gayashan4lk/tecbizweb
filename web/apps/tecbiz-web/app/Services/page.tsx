3fac40b61092af4fa1c3d01fcd6f881eb8f2224c
thilan <thilan@tecbizsolutions.com>
2023-10-24 23:15:25 +0530

    new

--- web/apps/tecbiz-web/app/Services/page.tsx
+++ web/apps/tecbiz-web/app/Services/page.tsx
@@ -4,44 +4,37 @@ const ServicesPage = () => {
   return (
     <div className="bg-black text-white">
       <div className="max-w-4xl mx-auto text-center py-24">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
-          BESPOKE WEB 2.0 DEVELOPMENT
-        </h1>
-        <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-8">
-          Our team of developers creates Web 2.0 products designed to meet specific business requirements. Leveraging our hands-on experience in the latest technologies like PostgreSQL, MongoDB, Angular, Node.js, React, React Native, and Python, we simplify complex business workflows with adaptive web and mobile apps.
-        </p>
-        <a href="#" className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition duration-200">
-          Contact Us
-        </a>
-      </div>
+        <div className="p-6 bg-black rounded-lg shadow-md text-blue-100 hover:bg-blue-200 hover:text-blue-900 transition duration-700 transform hover:scale-105">
+          <h2 className="text-3xl md:text-3xl font-bold text-center mb-8">BESPOKE WEB 2.0 DEVELOPMENT</h2>
+          <p className="text-xl md:text-xl font-semibold text-center mb-8">
+            Our team of developers creates Web 2.0 products designed to meet specific business requirements. Leveraging our hands-on experience in the latest technologies like PostgreSQL, MongoDB, Angular, Node.js, React, React Native, and Python, we simplify complex business workflows with adaptive web and mobile apps.
+          </p>
+          <a href="#" className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition duration-200">
+            Contact Us
+          </a>
+        </div>
 
+        <div className="p-6 bg-black rounded-lg shadow-md text-blue-100 hover:bg-blue-200 hover:text-blue-900 transition duration-700 transform hover:scale-105">
+          <h2 className="text-3xl md:text-3xl font-bold  text-center mb-8">AI CHATBOT DEVELOPMENT</h2>
+          <p className="text-xl md:text-xl font-semibold text-center mb-8">
+            We specialize in creating AI-powered chatbots for businesses. Our chatbots are designed to enhance customer interactions and streamline customer support. We leverage cutting-edge AI technologies to provide intelligent and efficient solutions for your business needs.
+          </p>
+          <a href="#" className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition duration-200">
+            Learn More
+          </a>
+        </div>
 
-      <div className="max-w-4xl mx-auto text-center py-16">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
-          AI CHATBOT DEVELOPMENT
-        </h1>
-        <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-8">
-          We specialize in creating AI-powered chatbots for businesses. Our chatbots are designed to enhance customer interactions and streamline customer support. We leverage cutting-edge AI technologies to provide intelligent and efficient solutions for your business needs.
-        </p>
-        <a href="#" className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition duration-200">
-          Learn More
-        </a>
-      </div>
-
-
-      <div className="max-w-4xl mx-auto text-center py-16">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
-          PYTHON-BASED APPLICATION DEVELOPMENT
-        </h1>
-        <p className="text-base sm:text-lg md:text-xl lg:text-2xl mb-8">
-          Our Python-based application development team excels in building robust and scalable applications. With Python's versatility, we create web applications, data analysis tools, automation scripts, and more. Let us harness the power of Python for your project.
-        </p>
-        <a href="#" className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition duration-200">
-          Learn More
-        </a>
+        <div className="p-6 bg-black rounded-lg shadow-md text-blue-100 hover:bg-blue-200 hover:text-blue-900 transition duration-700 transform hover:scale-105">
+          <h2 className="text-3xl md:text-3xl font-bold  text-center mb-8">PYTHON-BASED APPLICATION DEVELOPMENT</h2>
+          <p className="text-xl md:text-xl font-semibold text-center mb-8">
+            Our Python-based application development team excels in building robust and scalable applications. With Python's versatility, we create web applications, data analysis tools, automation scripts, and more. Let us harness the power of Python for your project.
+          </p>
+          <a href="#" className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-600 transition duration-200">
+            Learn More
+          </a>
+        </div>
       </div>
     </div>
   );
 };
-
 export default ServicesPage;
